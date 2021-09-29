7e8431f8c31bcd9a2d342b554f805afe3fda652e
Isaiah <isaiah.eichen@emerson.com>
2021-09-29 13:32:26 -0500

    #1 - stubbed out tests

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from lowball_rabbitmq_logging_handler.lowball_rabbitmq_logging_handler import pika
+from unittest.mock import Mock, MagicMock, PropertyMock
+
+from pika.channel import Channel
+
+
+@pytest.fixture
+def mock_pika_open_channel(monkeypatch):
+
+    mock_channel = Channel
+    monkeypatch.setattr(mock_channel, "__init__", Mock())
+    monkeypatch.setattr(mock_channel, "is_open", PropertyMock(return_value=True))
+    monkeypatch.setattr(mock_channel, "is_closed", PropertyMock(return_value=False))
+    monkeypatch.setattr(pika.BlockingConnection, "channel", Mock(return_value=mock_channel()))
+    monkeypatch.setattr(mock_channel, "basic_publish", Mock())
+    monkeypatch.setattr(mock_channel, "close", Mock())
+
+
+@pytest.fixture
+def mock_pika_closed_channel(monkeypatch):
+
+    mock_channel = Channel
+    monkeypatch.setattr(mock_channel, "__init__", Mock())
+    monkeypatch.setattr(mock_channel, "is_open", PropertyMock(return_value=False))
+    monkeypatch.setattr(mock_channel, "is_closed", PropertyMock(return_value=True))
+    monkeypatch.setattr(pika.BlockingConnection, "channel", Mock(return_value=mock_channel()))
+    monkeypatch.setattr(mock_channel, "basic_publish", Mock())
+    monkeypatch.setattr(mock_channel, "close", Mock())
+
+
+@pytest.fixture
+def mock_pika_open_blocking_connection(monkeypatch):
+
+    monkeypatch.setattr(pika.BlockingConnection, "__init__", Mock())
+    monkeypatch.setattr(pika.BlockingConnection, "is_open", PropertyMock(return_value=True))
+    monkeypatch.setattr(pika.BlockingConnection, "is_closed", PropertyMock(return_value=False))
+
+
+@pytest.fixture
+def mock_pika_closed_blocking_connection(monkeypatch):
+    monkeypatch.setattr(pika.BlockingConnection, "__init__", Mock())
+    monkeypatch.setattr(pika.BlockingConnection, "is_open", PropertyMock(return_value=False))
+    monkeypatch.setattr(pika.BlockingConnection, "is_closed", PropertyMock(return_value=True))
+
+
+
---
+++ tests/test_rmq_logging.py
@@ -0,0 +1,64 @@
+
+
+class TestLowballRabbitMQLoggingHandler:
+
+    def test_init_level(self):
+        pass
+
+    def test_init_host(self):
+
+        pass
+
+    def test_init_port(self):
+        pass
+
+    def test_init_username_password(self):
+        pass
+
+    def test_init_use_ssl(self):
+
+        pass
+
+    def test_init_verify_ssl(self):
+
+        pass
+
+    def test_init_ca_file(self):
+
+        pass
+
+    def test_init_ca_path(self):
+
+        pass
+
+    def test_init_exchange(self):
+
+        pass
+
+    def test_init_environment(self):
+
+        pass
+
+    def test_init_service_name(self):
+
+        pass
+
+    def test_init_formatter_configuration(self):
+
+        pass
+
+    def test_get_connection_parameters(self):
+
+        pass
+
+    def test_get_connection(self):
+
+        pass
+
+    def test_close_connection(self):
+
+        pass
+
+    def test_emit(self):
+
+        pass
